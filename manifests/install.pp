6979144c08a171b3bbed1b12a09bf8b2b1ccf430
root <root@dokorie1c.mylabserver.com>
2019-08-25 22:55:38 +0000

    to make installing apache on different OS easy

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,11 +4,8 @@
 #  Installs the base Apache package.
 # @example
 #   include apache::install
-class apache::install (
-  $install_name   = $apache::params::install_name,
-  $install_ensure = $apache::params::install_ensure,
-) inherits apache::params {
-  package { "$install_name":
-    ensure => $install_ensure,
+class apache::install {
+  package { "${apache::install_name}":
+    ensure => $apache::install_ensure,
   }
 }
